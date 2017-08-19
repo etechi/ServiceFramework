65f648a36973e81890f50bb8b2217811b5b96811
etechi <cy000@gmail.com>
2017-08-19 22:26:55 +0800

    move data model files

---
+++ Projects/Core/SF.Support/Data/Entity/CategoryEntityManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SF.Data.Storage;
+
+namespace SF.Data.Entity
+{
+
+	public abstract class CategoryEntityManager<
+		TCategoryKey,
+		TCategoryPublic,
+		TCategoryTemp,
+		TQueryArgument,
+		TCategoryEditable,
+		TCategoryModel,
+
+		TItemKey,
+		TItemModel
+		> :
+		EntityManager<TCategoryKey, TCategoryPublic, TCategoryTemp, TQueryArgument, TCategoryEditable, TCategoryModel>
+		where TCategoryPublic : class, IObjectWithId<TCategoryKey>
+		where TCategoryKey : IEquatable<TCategoryKey>
+		where TCategoryModel : class, IObjectWithId<TCategoryKey>, new()
+		where TQueryArgument : class, IQueryArgument<TCategoryKey>, new()
+		where TCategoryEditable : class, IObjectWithId<TCategoryKey>
+		where TItemKey : IEquatable<TItemKey>
+		where TItemModel : class, IObjectWithId<TItemKey>, new()
+	{
+		public CategoryEntityManager(
+			IDataSet<TCategoryModel> DataSet
+			) : base(DataSet)
+		{
+		}
+	}
+
+
+}
