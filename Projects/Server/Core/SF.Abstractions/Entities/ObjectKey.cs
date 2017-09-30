6b7ba65e32e86ac53ef59092077f32209b9676ca
etechi <cy000@gmail.com>
2017-09-30 14:31:43 +0800

    add key support

---
+++ Projects/Server/Core/SF.Abstractions/Entities/ObjectKey.cs
@@ -0,0 +1,26 @@
+﻿using SF.Metadata;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF.Entities
+{
+	public class ObjectKey<T>:
+		IEntityWithId<T>
+		 where T:IEquatable<T>
+
+	{
+		[Key]
+		[Comment("标识")]
+		[ReadOnly(true)]
+		[TableVisible]
+		public T Id { get; set; }
+
+		//public static implicit  operator ObjectKey<long>(long id)=>new ObjectKey<long> { Id=id};
+	}
+	
+}
---
+++ Projects/Server/Core/SF.Extensions/Entities/ObjectKey.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using System;
+using SF.Core.ServiceManagement;
+using System.Linq;
+using SF.Data;
+using System.Reflection;
+using System.Linq.Expressions;
+using System.Data.Common;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace SF.Entities
+{
+	public static class ObjectKey 
+	{
+		public static ObjectKey<T> From<T>(T Id)
+			where T : IEquatable<T>
+		{
+			return new ObjectKey<T> { Id = Id };
+		}
+	}
+}
