1698950f50a4a207bd37914540ca9a4b19476a13
ServiceFramework <cy-2015@foxmail.com>
2018-03-19 00:12:09 +0800

    excel export

--- Projects/Server/Utils/SF.Utils.TableExports.Excel/ExcelExporter.cs
+++ Projects/Server/Utils/SF.Utils.TableExports.Excel/ExcelExporter.cs
@@ -2,6 +2,8 @@
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Spreadsheet;
 using System;
+using X14 = DocumentFormat.OpenXml.Office2010.Excel;
+using X15 = DocumentFormat.OpenXml.Office2013.Excel;
 
 namespace SF.Utils.TableExports.Excel
 {
@@ -22,154 +24,146 @@ namespace SF.Utils.TableExports.Excel
         WorkbookPart WorkbookPart { get; }
         SpreadsheetDocument Document { get; }
         UInt32Value _rowIndex = new UInt32Value(0u);
-		private static Stylesheet CreateStylesheet()
-		{
-			Stylesheet ss = new Stylesheet();
 
-			//Fonts fts = new Fonts();
-			//DocumentFormat.OpenXml.Spreadsheet.Font ft = new DocumentFormat.OpenXml.Spreadsheet.Font();
-			//FontName ftn = new FontName();
-			//ftn.Val = "Calibri";
-			//FontSize ftsz = new FontSize();
-			//ftsz.Val = 11;
-			//ft.FontName = ftn;
-			//ft.FontSize = ftsz;
-			//fts.Append(ft);
-			//fts.Count = (uint)fts.ChildElements.Count;
+        Stylesheet CreateStylesheet()
+        {
+			Stylesheet stylesheet1 = new Stylesheet() { MCAttributes = new MarkupCompatibilityAttributes() { Ignorable = "x14ac x16r2" } };
+			stylesheet1.AddNamespaceDeclaration("mc", "http://schemas.openxmlformats.org/markup-compatibility/2006");
+			stylesheet1.AddNamespaceDeclaration("x14ac", "http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac");
+			stylesheet1.AddNamespaceDeclaration("x16r2", "http://schemas.microsoft.com/office/spreadsheetml/2015/02/main");
 
-			//Fills fills = new Fills();
-			//Fill fill;
-			//PatternFill patternFill;
-			//fill = new Fill();
-			//patternFill = new PatternFill();
-			//patternFill.PatternType = PatternValues.None;
-			//fill.PatternFill = patternFill;
-			//fills.Append(fill);
-			//fill = new Fill();
-			//patternFill = new PatternFill();
-			//patternFill.PatternType = PatternValues.Gray125;
-			//fill.PatternFill = patternFill;
-			//fills.Append(fill);
-			//fills.Count = (uint)fills.ChildElements.Count;
+			NumberingFormats numberingFormats1 = new NumberingFormats() { Count = (UInt32Value)1U };
+			NumberingFormat numberingFormat1 = new NumberingFormat() { NumberFormatId = (UInt32Value)178U, FormatCode = "yyyy\\-mm\\-dd\\ hh:mm" };
 
-			//Borders borders = new Borders();
-			//Border border = new Border();
-			//border.LeftBorder = new LeftBorder();
-			//border.RightBorder = new RightBorder();
-			//border.TopBorder = new TopBorder();
-			//border.BottomBorder = new BottomBorder();
-			//border.DiagonalBorder = new DiagonalBorder();
-			//borders.Append(border);
-			//borders.Count = (uint)borders.ChildElements.Count;
+			numberingFormats1.Append(numberingFormat1);
 
-			//CellStyleFormats csfs = new CellStyleFormats();
-			//CellFormat cf = new CellFormat();
-			//cf.NumberFormatId = 0;
-			//cf.FontId = 0;
-			//cf.FillId = 0;
-			//cf.BorderId = 0;
-			//csfs.Append(cf);
-			//csfs.Count = (uint)csfs.ChildElements.Count;
+			Fonts fonts1 = new Fonts() { Count = (UInt32Value)2U, KnownFonts = true };
 
-			//uint iExcelIndex = 164;
-			//NumberingFormats nfs = new NumberingFormats();
-			//CellFormats cfs = new CellFormats();
+			Font font1 = new Font();
+			FontSize fontSize1 = new FontSize() { Val = 11D };
+			Color color1 = new Color() { Theme = (UInt32Value)1U };
+			FontName fontName1 = new FontName() { Val = "等线" };
+			FontFamilyNumbering fontFamilyNumbering1 = new FontFamilyNumbering() { Val = 2 };
+			FontCharSet fontCharSet1 = new FontCharSet() { Val = 134 };
+			FontScheme fontScheme1 = new FontScheme() { Val = FontSchemeValues.Minor };
 
-			//cf = new CellFormat();
-			//cf.NumberFormatId = 0;
-			//cf.FontId = 0;
-			//cf.FillId = 0;
-			//cf.BorderId = 0;
-			//cf.FormatId = 0;
-			//cfs.Append(cf);
+			font1.Append(fontSize1);
+			font1.Append(color1);
+			font1.Append(fontName1);
+			font1.Append(fontFamilyNumbering1);
+			font1.Append(fontCharSet1);
+			font1.Append(fontScheme1);
 
-			//NumberingFormat nf;
-			//nf = new NumberingFormat();
-			//nf.NumberFormatId = iExcelIndex++;
-			//nf.FormatCode = "dd/mm/yyyy hh:mm:ss";
-			//nfs.Append(nf);
-			//cf = new CellFormat();
-			//cf.NumberFormatId = nf.NumberFormatId;
-			//cf.FontId = 0;
-			//cf.FillId = 0;
-			//cf.BorderId = 0;
-			//cf.FormatId = 0;
-			//cf.ApplyNumberFormat = true;
-			//cfs.Append(cf);
+			Font font2 = new Font();
+			FontSize fontSize2 = new FontSize() { Val = 9D };
+			FontName fontName2 = new FontName() { Val = "等线" };
+			FontFamilyNumbering fontFamilyNumbering2 = new FontFamilyNumbering() { Val = 2 };
+			FontCharSet fontCharSet2 = new FontCharSet() { Val = 134 };
+			FontScheme fontScheme2 = new FontScheme() { Val = FontSchemeValues.Minor };
 
-			//nf = new NumberingFormat();
-			//nf.NumberFormatId = iExcelIndex++;
-			//nf.FormatCode = "#,##0.0000";
-			//nfs.Append(nf);
-			//cf = new CellFormat();
-			//cf.NumberFormatId = nf.NumberFormatId;
-			//cf.FontId = 0;
-			//cf.FillId = 0;
-			//cf.BorderId = 0;
-			//cf.FormatId = 0;
-			//cf.ApplyNumberFormat = true;
-			//cfs.Append(cf);
+			font2.Append(fontSize2);
+			font2.Append(fontName2);
+			font2.Append(fontFamilyNumbering2);
+			font2.Append(fontCharSet2);
+			font2.Append(fontScheme2);
 
-			//// #,##0.00 is also Excel style index 4
-			//nf = new NumberingFormat();
-			//nf.NumberFormatId = iExcelIndex++;
-			//nf.FormatCode = "#,##0.00";
-			//nfs.Append(nf);
-			//cf = new CellFormat();
-			//cf.NumberFormatId = nf.NumberFormatId;
-			//cf.FontId = 0;
-			//cf.FillId = 0;
-			//cf.BorderId = 0;
-			//cf.FormatId = 0;
-			//cf.ApplyNumberFormat = true;
-			//cfs.Append(cf);
+			fonts1.Append(font1);
+			fonts1.Append(font2);
 
-			//// @ is also Excel style index 49
-			//nf = new NumberingFormat();
-			//nf.NumberFormatId = iExcelIndex++;
-			//nf.FormatCode = "@";
-			//nfs.Append(nf);
-			//cf = new CellFormat();
-			//cf.NumberFormatId = nf.NumberFormatId;
-			//cf.FontId = 0;
-			//cf.FillId = 0;
-			//cf.BorderId = 0;
-			//cf.FormatId = 0;
-			//cf.ApplyNumberFormat = true;
-			//cfs.Append(cf);
+			Fills fills1 = new Fills() { Count = (UInt32Value)2U };
 
-			//nfs.Count = (uint)nfs.ChildElements.Count;
-			//cfs.Count = (uint)cfs.ChildElements.Count;
+			Fill fill1 = new Fill();
+			PatternFill patternFill1 = new PatternFill() { PatternType = PatternValues.None };
 
-			//ss.Append(nfs);
-			//ss.Append(fts);
-			//ss.Append(fills);
-			//ss.Append(borders);
-			//ss.Append(csfs);
-			//ss.Append(cfs);
+			fill1.Append(patternFill1);
 
-			//CellStyles css = new CellStyles();
-			//CellStyle cs = new CellStyle();
-			//cs.Name = "Normal";
-			//cs.FormatId = 0;
-			//cs.BuiltinId = 0;
-			//css.Append(cs);
-			//css.Count = (uint)css.ChildElements.Count;
-			//ss.Append(css);
+			Fill fill2 = new Fill();
+			PatternFill patternFill2 = new PatternFill() { PatternType = PatternValues.Gray125 };
 
-			//DifferentialFormats dfs = new DifferentialFormats();
-			//dfs.Count = 0;
-			//ss.Append(dfs);
+			fill2.Append(patternFill2);
 
-			//TableStyles tss = new TableStyles();
-			//tss.Count = 0;
-			//tss.DefaultTableStyle = "TableStyleMedium9";
-			//tss.DefaultPivotStyle = "PivotStyleLight16";
-			//ss.Append(tss);
+			fills1.Append(fill1);
+			fills1.Append(fill2);
 
-			return ss;
+			Borders borders1 = new Borders() { Count = (UInt32Value)1U };
+
+			Border border1 = new Border();
+			LeftBorder leftBorder1 = new LeftBorder();
+			RightBorder rightBorder1 = new RightBorder();
+			TopBorder topBorder1 = new TopBorder();
+			BottomBorder bottomBorder1 = new BottomBorder();
+			DiagonalBorder diagonalBorder1 = new DiagonalBorder();
+
+			border1.Append(leftBorder1);
+			border1.Append(rightBorder1);
+			border1.Append(topBorder1);
+			border1.Append(bottomBorder1);
+			border1.Append(diagonalBorder1);
+
+			borders1.Append(border1);
+
+			CellStyleFormats cellStyleFormats1 = new CellStyleFormats() { Count = (UInt32Value)1U };
+
+			CellFormat cellFormat1 = new CellFormat() { NumberFormatId = (UInt32Value)0U, FontId = (UInt32Value)0U, FillId = (UInt32Value)0U, BorderId = (UInt32Value)0U };
+			Alignment alignment1 = new Alignment() { Vertical = VerticalAlignmentValues.Center };
+
+			cellFormat1.Append(alignment1);
+
+			cellStyleFormats1.Append(cellFormat1);
+
+			CellFormats cellFormats1 = new CellFormats() { Count = (UInt32Value)2U };
+
+			CellFormat cellFormat2 = new CellFormat() { NumberFormatId = (UInt32Value)0U, FontId = (UInt32Value)0U, FillId = (UInt32Value)0U, BorderId = (UInt32Value)0U, FormatId = (UInt32Value)0U };
+			Alignment alignment2 = new Alignment() { Vertical = VerticalAlignmentValues.Center };
+
+			cellFormat2.Append(alignment2);
+
+			CellFormat cellFormat3 = new CellFormat() { NumberFormatId = (UInt32Value)178U, FontId = (UInt32Value)0U, FillId = (UInt32Value)0U, BorderId = (UInt32Value)0U, FormatId = (UInt32Value)0U, ApplyNumberFormat = true };
+			Alignment alignment3 = new Alignment() { Vertical = VerticalAlignmentValues.Center };
+
+			cellFormat3.Append(alignment3);
+
+			cellFormats1.Append(cellFormat2);
+			cellFormats1.Append(cellFormat3);
+
+			CellStyles cellStyles1 = new CellStyles() { Count = (UInt32Value)1U };
+			CellStyle cellStyle1 = new CellStyle() { Name = "常规", FormatId = (UInt32Value)0U, BuiltinId = (UInt32Value)0U };
+
+			cellStyles1.Append(cellStyle1);
+			DifferentialFormats differentialFormats1 = new DifferentialFormats() { Count = (UInt32Value)0U };
+			TableStyles tableStyles1 = new TableStyles() { Count = (UInt32Value)0U, DefaultTableStyle = "TableStyleMedium2", DefaultPivotStyle = "PivotStyleLight16" };
+
+			StylesheetExtensionList stylesheetExtensionList1 = new StylesheetExtensionList();
+
+			StylesheetExtension stylesheetExtension1 = new StylesheetExtension() { Uri = "{EB79DEF2-80B8-43e5-95BD-54CBDDF9020C}" };
+			stylesheetExtension1.AddNamespaceDeclaration("x14", "http://schemas.microsoft.com/office/spreadsheetml/2009/9/main");
+			X14.SlicerStyles slicerStyles1 = new X14.SlicerStyles() { DefaultSlicerStyle = "SlicerStyleLight1" };
+
+			stylesheetExtension1.Append(slicerStyles1);
+
+			StylesheetExtension stylesheetExtension2 = new StylesheetExtension() { Uri = "{9260A510-F301-46a8-8635-F512D64BE5F5}" };
+			stylesheetExtension2.AddNamespaceDeclaration("x15", "http://schemas.microsoft.com/office/spreadsheetml/2010/11/main");
+			X15.TimelineStyles timelineStyles1 = new X15.TimelineStyles() { DefaultTimelineStyle = "TimeSlicerStyleLight1" };
+
+			stylesheetExtension2.Append(timelineStyles1);
+
+			stylesheetExtensionList1.Append(stylesheetExtension1);
+			stylesheetExtensionList1.Append(stylesheetExtension2);
+
+			stylesheet1.Append(numberingFormats1);
+			stylesheet1.Append(fonts1);
+			stylesheet1.Append(fills1);
+			stylesheet1.Append(borders1);
+			stylesheet1.Append(cellStyleFormats1);
+			stylesheet1.Append(cellFormats1);
+			stylesheet1.Append(cellStyles1);
+			stylesheet1.Append(differentialFormats1);
+			stylesheet1.Append(tableStyles1);
+			stylesheet1.Append(stylesheetExtensionList1);
+
+			return stylesheet1;
 		}
-        public ExcelExporter(System.IO.Stream Stream,string Title, Column[] Columns):base(Columns)
+		public ExcelExporter(System.IO.Stream Stream,string Title, Column[] Columns):base(Columns)
         {
             this.Title = string.IsNullOrWhiteSpace(Title) ? "导出数据" : Title;
 
@@ -180,10 +174,10 @@ namespace SF.Utils.TableExports.Excel
             var worksheetPart = WorkbookPart.AddNewPart<WorksheetPart>();
 
 			// Style Part
-			var wbsp = WorkbookPart.AddNewPart<WorkbookStylesPart>();
+			WorkbookStylesPart wbsp = WorkbookPart.AddNewPart<WorkbookStylesPart>();
 			wbsp.Stylesheet = CreateStylesheet();
-			//wbsp.Stylesheet.Save();
-			
+			wbsp.Stylesheet.Save();
+
 			this.SheetData = new SheetData();
 
             worksheetPart.Worksheet = new Worksheet(this.SheetData);
@@ -318,10 +312,10 @@ namespace SF.Utils.TableExports.Excel
                 case TypeCode.DateTime:
 					{
 						//cell.DataType = CellValues.Date;
-						//cell.Append(new CellValue(Convert.ToDateTime(text).ToOADate().ToString()));
-						//cell.StyleIndex = 164u;
+						cell.Append(new CellValue(Convert.ToDateTime(text).ToOADate().ToString()));
+						cell.StyleIndex = 1u;
 
-						SetText(cell, text);
+						//SetText(cell, text);
                     }
                     break;
                 case TypeCode.DBNull:
